23fc9ae88f7cd239f036b149fd3c07ecf0657ba6
Nikita Chikunov <ninjaken32@gmail.com>
2021-06-22 02:17:41 +0500

    finish

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-import * as express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+const express = require('express');
+const http = require('http');
+const io = require('socket.io');
+const admin = require('firebase-admin');
+const firebaseKeys = require('./keys/serviceAccountKey.json');
 
-const port = 8000;
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseKeys),
+});
+
+const port = 3000;
 const app = express();
 
 app.use(express.json());
@@ -10,10 +16,28 @@ app.use(express.urlencoded({ extended: false }))
 
 app.set('port', port);
 
-const server = createServer(app);
+const server = http.createServer(app);
 
-const io = new Server(server);
+const ioServer = new io.Server(server);
 
-server.listen(port);
+ioServer.on('connection', async socket => {
+  const token = socket.handshake.headers.authorization.split(' ')[1];
+  const verifiedToken = await admin.auth().verifyIdToken(token);
 
-server.listen(port);
+  socket.broadcast.emit('user_connected', { id: verifiedToken.uid, email: verifiedToken.email })
+
+  socket.on('user_disconnected', user => socket.broadcast.emit('user_disconnected', user));
+
+  socket.on('send_message', message => socket.broadcast.emit('received_message', message));
+
+  socket.on('typing', data => socket.broadcast.emit('typing', data));
+
+  socket.on('stop_typing', data => socket.broadcast.emit('stop_typing', data));
+});
+
+server.listen(port, err => {
+  if (err) throw err;
+
+  console.log(`Listening on port ${port}`)
+});
+
